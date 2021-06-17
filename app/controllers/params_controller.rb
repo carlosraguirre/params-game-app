e83b92a528082d9d3975e53a5efdb807348ddf23
Carlos Aguirre <craguirre952@gmail.com>
2021-06-16 20:28:37 -0400

    add params_game and params_number_game methods

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,2 +1,26 @@
 class ParamsController < ApplicationController
+  
+  def params_game
+    first_name = params[:the_first_name].upcase
+    first_letter - first_name[0]
+    if first_letter == "A"
+      the_message = "your name begins with 'A'"
+    else
+      the_message = "hello #{first_name}"
+    end
+    render json: {message: the_message}
+  end
+
+  def params_number_game
+    user_input = params[:user_guess].to_i
+    answer = 36
+    if user_input> answer
+      message = "you guess too high"
+    elsif user_input < answer
+      message = "you guessed too low"
+    else
+      message = "you are correct"
+    end
+    render json: {message: message}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/params_game", controller: "params", action: "params_game"
+
+  get "/params_number_game", controller: "params", action: "params_number_game"
 end
